573a9c50c8fa44ef328cf54bede57a1c6a309392
Vadim Kozhukh <doc.kozhukh@gmail.com>
2026-01-28 08:55:53 +0200

    fix error

--- main.tf
+++ main.tf
@@ -1,35 +1,15 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "064b71f3",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "provider \"aws\" {\n",
-    "  region = \"us-east-1\"\n",
-    "}\n",
-    "\n",
-    "resource \"aws_security_group\" \"user_api\" {\n",
-    "  name        = \"user-api-sg\"\n",
-    "  description = \"Allow 443 from anywhere\"\n",
-    "\n",
-    "  ingress {\n",
-    "    from_port   = 443\n",
-    "    to_port     = 443\n",
-    "    protocol    = \"tcp\"\n",
-    "    cidr_blocks = [\"0.0.0.0/0\"]  # ❌ відкрита зона\n",
-    "  }\n",
-    "}"
-   ]
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "user_api" {
+  name        = "user-api-sg"
+  description = "Allow 443 from anywhere"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # ❌ відкрита зона
   }
- ],
- "metadata": {
-  "language_info": {
-   "name": "python"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
 }
